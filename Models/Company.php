73be4b191bdd505ea40cb9acac24ef633205c78e
Sorethea OK <sorethea@gmail.com>
2023-02-06 16:11:38 +0700

    first commit

--- Models/Company.php
+++ Models/Company.php
@@ -4,15 +4,21 @@ namespace Modules\HR\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Company extends Model
 {
     use HasFactory;
 
     protected $fillable = [];
-    
+
     protected static function newFactory()
     {
         return \Modules\HR\Database\factories\CompanyFactory::new();
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class,'parent_id');
     }
 }
