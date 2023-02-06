1fc44371817455bd1b2af961a4247eed5b406f91
Sorethea OK <sorethea@gmail.com>
2023-02-06 16:21:40 +0700

    lam-dev

--- Filament/Resources/CompanyResource.php
+++ Filament/Resources/CompanyResource.php
@@ -41,15 +41,16 @@ class CompanyResource extends Resource
                     Forms\Components\BelongsToSelect::make("parent")
                         ->relationship("parent","name")
                         ->createOptionForm([
-                            Forms\Components\Card::make([
-                                Forms\Components\TextInput::make("name")
-                                    ->unique("companies","name",fn($record)=>$record)
-                                    ->required(),
-                                Forms\Components\TextInput::make("abbr")
-                                    ->unique("companies","abbr",fn($record)=>$record)
-                                    ->required(),
-                            ])->columns(2),
-                        ])
+//                            Forms\Components\Card::make([
+//
+//                            ])->columns(2),
+                            Forms\Components\TextInput::make("name")
+                                ->unique("companies","name",fn($record)=>$record)
+                                ->required(),
+                            Forms\Components\TextInput::make("abbr")
+                                ->unique("companies","abbr",fn($record)=>$record)
+                                ->required(),
+                        ])->columns(2)
                         ->nullable(),
                     Forms\Components\TextInput::make("domain")
                         ->nullable(),
