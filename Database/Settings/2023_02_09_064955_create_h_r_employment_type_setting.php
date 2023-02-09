1535fd92a6558beb404c5fbe38509e4033d933be
Sorethea OK <sorethea@gmail.com>
2023-02-09 13:54:48 +0700

    lam-dev

---
+++ Database/Settings/2023_02_09_064955_create_h_r_employment_type_setting.php
@@ -0,0 +1,15 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+class CreateHREmploymentTypeSetting extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('hr.employment_type',[
+            'contract'=>'Contract',
+            'full-time'=>'Full Time',
+            'part-time'=>'Part Time',
+        ]);
+    }
+}
