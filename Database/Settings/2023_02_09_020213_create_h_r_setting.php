0f2ff897aa04dd217b974cd15911c48a9bcc49e3
Sorethea OK <sorethea@gmail.com>
2023-02-09 11:19:03 +0700

    lam-dev

--- Database/Settings/2023_02_09_020213_create_h_r_setting.php
+++ Database/Settings/2023_02_09_020213_create_h_r_setting.php
@@ -6,6 +6,14 @@ class CreateHRSetting extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add("hr.gender",["male"=>"Male","female"=>"Female"]);
+        $this->migrator->add("hr.gender",[
+            "male"=>"Male",
+            "female"=>"Female"
+        ]);
+        $this->migrator->add("hr.employee_type",[
+            "contract"=>"Contract",
+            "full-time"=>"Full Time",
+            "part-time"=>"Part Time",
+            ]);
     }
 }
