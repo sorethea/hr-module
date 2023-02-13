faa3fee77aba63fc9c088acec23f0e9f6b2ac1ee
Sorethea OK <sorethea@gmail.com>
2023-02-13 10:14:48 +0700

    lam-dev

--- Filament/Resources/EmployeeResource.php
+++ Filament/Resources/EmployeeResource.php
@@ -61,16 +61,16 @@ class EmployeeResource extends Resource
                         ->reactive()
                         ->default(false),
                     Forms\Components\TextInput::make("email")
-                        ->visible(fn($record,\Closure $get)=>$get("is_system_user") && !$record->user)
+                        ->visible(fn($record,\Closure $get)=>$get("is_system_user") && empty($record->user))
                         ->required(),
                     Forms\Components\TextInput::make("password")
                         ->password()
                         ->required()
-                        ->visible(fn($record,\Closure $get)=>$get("is_system_user") && !$record->user)
+                        ->visible(fn($record,\Closure $get)=>$get("is_system_user") && empty($record->user))
                         ->same("password_confirmation"),
                     Forms\Components\TextInput::make("password_confirmation")
                         ->password()
-                        ->visible(fn($record,\Closure $get)=>$get("is_system_user") && !$record->user)
+                        ->visible(fn($record,\Closure $get)=>$get("is_system_user") && empty($record->user))
                         ->required(),
                     Forms\Components\Select::make("gender")
                         ->options(app(HRSetting::class)->gender)
