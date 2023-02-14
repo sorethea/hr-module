59447dad6d98e38687f1a6978125efdab92a306e
Sorethea OK <sorethea@gmail.com>
2023-02-14 15:47:48 +0700

    lam-dev

---
+++ Providers/UninstallServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\HR\Providers;
+
+use Modules\Core\Providers\BaseUninstallServiceProvider;
+use Modules\HR\Database\Migrations\CreateCompaniesTable;
+use Modules\HR\Database\Migrations\CreateEmployeesTable;
+use Modules\HR\Database\Seeders\HRDatabaseSeeder;
+use Modules\HR\Database\Settings\CreateHREmploymentTypeSetting;
+use Modules\HR\Database\Settings\CreateHRGenderSetting;
+
+class UninstallServiceProvider extends BaseUninstallServiceProvider
+{
+    protected $migrations = [
+        CreateEmployeesTable::class,
+        CreateCompaniesTable::class,
+        CreateHRGenderSetting::class,
+        CreateHREmploymentTypeSetting::class,
+    ];
+    public function install()
+    {
+        $this->dropSchema();
+        $seed =new HRDatabaseSeeder();
+        $seed->rollback();
+    }
+}
