1f4957c1ef7be984a3dd5b57e444fceabed4ea63
Sorethea OK <sorethea@gmail.com>
2023-02-21 15:35:13 +0700

    lam-dev

--- Providers/InstallServiceProvider.php
+++ Providers/InstallServiceProvider.php
@@ -4,26 +4,20 @@ namespace Modules\HR\Providers;
 
 use Closure;
 
-use Illuminate\Support\ServiceProvider;
-use Modules\Core\Providers\BaseInstallServiceProvider;
+
 use Modules\HR\Database\Migrations\CreateCompaniesTable;
 use Modules\HR\Database\Migrations\CreateEmployeesTable;
 use Modules\HR\Database\Seeders\HRDatabaseSeeder;
 use Modules\HR\Database\Settings\CreateHREmploymentTypeSetting;
 use Modules\HR\Database\Settings\CreateHRGenderSetting;
+use Modules\LAM\Providers\BaseInstallServiceProvider;
 
 
 class InstallServiceProvider extends BaseInstallServiceProvider
 {
-    protected $migrations = [
-        CreateEmployeesTable::class,
-        CreateCompaniesTable::class,
-        CreateHRGenderSetting::class,
-        CreateHREmploymentTypeSetting::class,
-    ];
+
     public function install()
     {
-        $this->createSchema();
         $seed =new HRDatabaseSeeder();
         $seed->run();
     }
--- Providers/UninstallServiceProvider.php
+++ Providers/UninstallServiceProvider.php
@@ -2,26 +2,19 @@
 
 namespace Modules\HR\Providers;
 
-use Modules\Core\Providers\BaseUninstallServiceProvider;
 use Modules\HR\Database\Migrations\CreateCompaniesTable;
 use Modules\HR\Database\Migrations\CreateEmployeesTable;
 use Modules\HR\Database\Seeders\HRDatabaseSeeder;
 use Modules\HR\Database\Settings\CreateHREmploymentTypeSetting;
 use Modules\HR\Database\Settings\CreateHRGenderSetting;
+use Modules\LAM\Providers\BaseUninstallServiceProvider;
 
 class UninstallServiceProvider extends BaseUninstallServiceProvider
 {
-    protected $migrations = [
-        CreateEmployeesTable::class,
-        CreateCompaniesTable::class,
-        CreateHRGenderSetting::class,
-        CreateHREmploymentTypeSetting::class,
-    ];
+
     public function uninstall()
     {
-
         $seed =new HRDatabaseSeeder();
         $seed->rollback();
-        $this->dropSchema();
     }
 }
