0a9c1099c6ab400d0d96cf3ec31e3afaaa4a524f
Sorethea OK <sorethea@gmail.com>
2023-02-24 15:00:55 +0700

    lam-dev

--- Settings/HRSetting.php
+++ Settings/HRSetting.php
@@ -7,6 +7,7 @@ class HRSetting extends Settings
 {
     public array $gender = [];
     public array $employment_type =[];
+    public array $leave_type=[];
 
     public static function group(): string
     {
