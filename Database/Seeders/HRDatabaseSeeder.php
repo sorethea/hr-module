774543a1e6d6a34bbdbfdf3c855ae208a8ff1587
Sorethea OK <sorethea@gmail.com>
2023-03-01 15:49:43 +0700

    lam-dev

--- Database/Seeders/HRDatabaseSeeder.php
+++ Database/Seeders/HRDatabaseSeeder.php
@@ -19,8 +19,8 @@ class HRDatabaseSeeder extends Seeder
     }
     public function rollback()
     {
-        info("uninstall HR module");
         $permissionTableSeeder = new PermissionTableSeeder();
         $permissionTableSeeder->rollback();
+        info("uninstall HR module");
     }
 }
