21eb807d80d8a5fc8777847c469a5e397411e3a5
Sorethea OK <sorethea@gmail.com>
2023-03-03 15:09:32 +0700

    lam-dev

--- Filament/Resources/HolidayResource/RelationManagers/HolidayDatesRelationManager.php
+++ Filament/Resources/HolidayResource/RelationManagers/HolidayDatesRelationManager.php
@@ -47,7 +47,7 @@ class HolidayDatesRelationManager extends RelationManager
                     ->color('success')
                     ->label("New weekly holiday")
                     ->action(function (){
-                        info(json_encode(request()->get("ownerRecord")));
+                        info(json_encode(request()->get("data")));
                     })
                     ->form([
                         Forms\Components\Group::make([
