60ac9c1f0b3082e22a4fe632e5a078e2fa77dfb6
Sorethea OK <sorethea@gmail.com>
2023-03-07 15:03:57 +0700

    lam-dev

--- Filament/Resources/HolidayResource/RelationManagers/HolidayDatesRelationManager.php
+++ Filament/Resources/HolidayResource/RelationManagers/HolidayDatesRelationManager.php
@@ -50,6 +50,7 @@ class HolidayDatesRelationManager extends RelationManager
                     ->action(function (RelationManager $livewire, array $data){
                         $holiday = $livewire->ownerRecord;
                         \Holiday::generateWeekDayHolidays($holiday,$data["half_day"],$data["weekly_off"]);
+                        redirect(request()->header("Referer"));
                     })
                     ->form([
                         Forms\Components\Group::make([
