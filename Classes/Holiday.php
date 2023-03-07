5bde021390853305b07e7870f4b48fdf39e0558c
Sorethea OK <sorethea@gmail.com>
2023-03-07 10:52:03 +0700

    lam-dev

--- Classes/Holiday.php
+++ Classes/Holiday.php
@@ -3,6 +3,7 @@
 namespace Modules\HR\Classes;
 
 
+use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Model;
 use Modules\HR\Models\HolidayDate;
@@ -26,11 +27,13 @@ class Holiday
     {
         return self::$isDayOfWeekList;
     }
-    public static function generateWeekDayHolidays(Model $holiday, bool $isHalfDay,string $isDayOfWeek,string $dayOfWeek): void
+    public static function generateWeekDayHolidays(Model $holiday, bool $isHalfDay, string $isDayOfWeek, string $dayOfWeek): void
     {
         $fromDate = $holiday->from_date;
         $toDate = $holiday->to_date;
-        $dates =CarbonPeriod::between($fromDate,$toDate)->filter($isDayOfWeek);
+        $dates =CarbonPeriod::between($fromDate,$toDate)->filter(function (Carbon $date,string $isDayOfWeek){
+            return $date->$isDayOfWeek();
+        });
         foreach ($dates as $date){
             HolidayDate::firstOrCreate([
                 'holiday_id' => $holiday->id,
