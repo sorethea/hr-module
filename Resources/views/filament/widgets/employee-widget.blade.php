7284f8baf5a51b28d2aca4fc29078c4d75137877
Sorethea OK <sorethea@gmail.com>
2023-03-13 15:29:22 +0700

    lam-dev

--- Resources/views/filament/widgets/employee-widget.blade.php
+++ Resources/views/filament/widgets/employee-widget.blade.php
@@ -5,12 +5,7 @@
             @endphp
 
             <div class="h-12 flex items-center space-x-4 rtl:space-x-reverse">
-                <x-hr::user-avatar :user="$user" />
-{{--                @if($user->getFirstMediaUrl('avatar'))--}}
-{{--                    <img src="{{$user->getFirstMediaUrl('avatar')}}" class="rounded-full w-12 mb-2"/>--}}
-{{--                @else--}}
-{{--                    <x-filament::user-avatar :user="$user" />--}}
-{{--                @endif--}}
+                <x-hr::employee-avatar :user="$user" />
                 <div>
                     <h2 class="text-lg sm:text-xl font-bold tracking-tight">
                         {{ __('filament::widgets/account-widget.welcome', ['user' => \Filament\Facades\Filament::getUserName($user)]) }}
