85a016a87998afcf6a300d076bcb456e315d5031
Sorethea OK <sorethea@gmail.com>
2023-03-13 16:37:30 +0700

    lam-dev

--- Models/Department.php
+++ Models/Department.php
@@ -4,15 +4,20 @@ namespace Modules\HR\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Department extends Model
 {
     use HasFactory;
 
     protected $fillable = [];
-    
+
     protected static function newFactory()
     {
         return \Modules\HR\Database\factories\DepartmentFactory::new();
+    }
+
+    public function parent(): BelongsTo{
+        return $this->belongsTo(self::class,'parent_id','id');
     }
 }
