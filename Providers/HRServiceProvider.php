091b9420fe1e4e382dca1ed41e1cad451b3c9b96
Sorethea OK <sorethea@gmail.com>
2023-03-13 10:27:24 +0700

    lam-dev

--- Providers/HRServiceProvider.php
+++ Providers/HRServiceProvider.php
@@ -108,4 +108,15 @@ class HRServiceProvider extends ServiceProvider
     {
         return [];
     }
+
+    private function getPublishableViewPaths(): array
+    {
+        $paths = [];
+        foreach (\Config::get('view.paths') as $path) {
+            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
+                $paths[] = $path . '/modules/' . $this->moduleNameLower;
+            }
+        }
+        return $paths;
+    }
 }
