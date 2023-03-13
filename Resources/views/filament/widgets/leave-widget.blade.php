d858dd1fed6a0d16faa9e7fe2227dbd995ac2982
Sorethea OK <sorethea@gmail.com>
2023-03-13 10:59:21 +0700

    lam-dev

--- Resources/views/filament/widgets/leave-widget.blade.php
+++ Resources/views/filament/widgets/leave-widget.blade.php
@@ -1,5 +1,5 @@
 <x-filament::widget>
     <x-filament::card>
-        <h1>Leave Widget</h1>
+        <h2 class="text-lg sm:text-xl font-bold tracking-tight">Leave Widget</h2>
     </x-filament::card>
 </x-filament::widget>
