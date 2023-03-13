7c53e25c26a2278aa8e8485e474d8ba3a0ba2221
Sorethea OK <sorethea@gmail.com>
2023-03-13 11:34:06 +0700

    lam-dev

--- Resources/views/filament/widgets/leave-widget.blade.php
+++ Resources/views/filament/widgets/leave-widget.blade.php
@@ -1,8 +1,14 @@
 <x-filament::widget>
     <x-filament::card>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-        <h2 class="text-lg sm:text-xl font-bold tracking-tight">Leave Widget</h2>
+        <div>
+            <x-heroicon-o-dots-vertical>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </x-heroicon-o-dots-vertical>
+
+            <h2 class="text-lg sm:text-xl font-bold tracking-tight">Leave Widget</h2>
+        </div>
+
     </x-filament::card>
 </x-filament::widget>
