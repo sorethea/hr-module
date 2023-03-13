90c8bc87f9a2affe71ead34a51d93520ed190db6
Sorethea OK <sorethea@gmail.com>
2023-03-13 09:51:30 +0700

    lam-dev

---
+++ Filament/Widgets/EmployeeWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\HR\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class EmployeeWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.employee-widget';
+}
