059f1df41bb518cf1a7e81ca75dd94db8e4e4ea0
Sorethea OK <sorethea@gmail.com>
2023-03-13 09:39:07 +0700

    lam-dev

--- Filament/Widgets/EmployeeWidget.php
+++ Filament/Widgets/EmployeeWidget.php
@@ -6,5 +6,6 @@ use Filament\Widgets\Widget;
 
 class EmployeeWidget extends Widget
 {
+    protected static ?int $sort = -2;
     protected static string $view = 'filament.widgets.employee-widget';
 }
