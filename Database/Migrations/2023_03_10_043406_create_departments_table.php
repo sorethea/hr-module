79d9097af7603403dccc0007ef54549ce0ad60d7
Sorethea OK <sorethea@gmail.com>
2023-03-14 10:26:21 +0700

    lam-dev

--- Database/Migrations/2023_03_10_043406_create_departments_table.php
+++ Database/Migrations/2023_03_10_043406_create_departments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->foreignId("company_id");
+            $table->foreignId("company_id")->nullable();
             $table->foreignId("parent_id")->nullable();
             $table->boolean("is_group")->default(false);
             $table->boolean("active")->default(true);
