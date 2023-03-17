1cb7d414b957a17732bce46fdf6588eb1b7d239e
Sorethea OK <sorethea@gmail.com>
2023-03-17 09:56:20 +0700

    lam-dev

--- Resources/views/filament/widgets/h-r-widget.blade.php
+++ Resources/views/filament/widgets/h-r-widget.blade.php
@@ -1,5 +1,13 @@
 <x-filament::widget>
     <x-filament::card>
         {{-- Widget content --}}
+        <div class="h-12 flex items-center space-x-4 rtl:space-x-reverse">
+            <x-hr::employee-avatar :user="$user" />
+            <div>
+                <h2 class="text-lg sm:text-xl font-bold tracking-tight">
+                    {{ \Lam::getEmployeeName($user)}}
+                </h2>
+            </div>
+        </div>
     </x-filament::card>
 </x-filament::widget>
